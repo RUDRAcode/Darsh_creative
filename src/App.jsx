9c8c84dec88d7ea8fea7fdabb5e4093589764b2d
RudrapravaChatterjee <rudrachatterjee864@gmail.com>
2024-12-25 13:57:55 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { useState, React, Fragment } from "react";
 import "./App.css";
-
 import OwlSide from "./components/OwlSide";
 import TigerSide from "./components/TigerSide";
 import { AnimatePresence } from "motion/react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar.jsx"
+import drash from "./assets/darsh.png"
 
 
 
 function App() {
   const [open, setOpen] = useState(true);
+  const backgroundStyle = {
+     backgroundImage: `url(${drash})`,
+    backgroundSize: 'cover',  
+    backgroundRepeat: 'no-repeat', 
+    backgroundPosition: 'center', 
+    height: '100vh', 
+    width: '100%', 
+  }
   return (
     <>
       <div className="w-full h-svh flex flex-col relative">
       <Navbar/>
         <AnimatePresence>
-          <div className="flex-1 lg:hidden bg-edu-pattern object-cover">
+          <div className="flex-1 lg:hidden"style={backgroundStyle}>
             {open ? (
               <OwlSide setOpen={setOpen}  />
             ) : (
